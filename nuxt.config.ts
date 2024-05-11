eb63302b82e51d2d4a00c097cde40ef841a2bd02
Kauê de Magalhães <kauedemagalhaes27@gmail.com>
2024-05-11 01:26:14 -0300

    installing tailwindcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+    devtools: {enabled: true},
+    modules: ["@nuxtjs/tailwindcss"],
+    app: {
+        head: {
+            bodyAttrs: {
+                class: "bg-zinc-900 text-white font-semibold font-mono",
+            },
+        },
+    },
 })
